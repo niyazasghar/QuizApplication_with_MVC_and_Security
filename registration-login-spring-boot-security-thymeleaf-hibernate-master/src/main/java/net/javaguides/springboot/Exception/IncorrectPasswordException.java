6ae95038df59ea7864134e5f905a234b25ffe2e0
niyazasghar <niyazasghar12@gmail.com>
2024-08-02 15:27:21 +0530

    Delete registration-login-spring-boot-security-thymeleaf-hibernate-master directory

--- registration-login-spring-boot-security-thymeleaf-hibernate-master/src/main/java/net/javaguides/springboot/Exception/IncorrectPasswordException.java
+++
@@ -1,7 +0,0 @@
-package net.javaguides.springboot.Exception;
-
-public class IncorrectPasswordException extends RuntimeException {
-    public IncorrectPasswordException(String message) {
-        super(message);
-    }
-}
